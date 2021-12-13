b02fe8d81b245420a9e115bcde7985e50c8a5594
Szkola <Szkola@S24K01>
2021-12-13 12:22:01 +0100

    Dodaj pliki projektów.

---
+++ Lista produktów2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Lista_produktów
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Produkt> ListaProduktow = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrzygotujWizanie();
+        }
+        private void PrzygotujWizanie()
+        {
+            ListaProduktow = new ObservableCollection<Produkt>();
+            ListaProduktow.Add(new Produkt("01-11", "ołówek", 8, "Katowice 1"));
+            ListaProduktow.Add(new Produkt("PW-20", "pióro wieczne", 75, "Katowice 2"));
+            ListaProduktow.Add(new Produkt("DZ-10", "długopis żelowy", 1121, "Katowice 1"));
+            ListaProduktow.Add(new Produkt("DZ-12", "długopis kulowy", 280, "Katowice 2"));
+            lstProdukty.ItemsSource = ListaProduktow;
+
+            CollectionView widok = (CollectionView)CollectionViewSource.GetDefaultView(lstProdukty.ItemsSource);
+            widok.SortDescriptions.Add(new SortDescription("Magazyn", ListSortDirection.Descending));
+            widok.SortDescriptions.Add(new SortDescription("Nazwa", ListSortDirection.Ascending));
+        }
+    }
+
+}
---
+++ Lista produktów2/Produkt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista_produktów
+{
+    class Produkt
+    {
+        public string Symbol { get; set; }
+        public string Nazwa { get; set; }
+        public int LiczbaSztuk { get; set; }
+        public string Magazyn { get; set; }
+        public Produkt(string sym, string naz, int lszt, string mag)
+        {
+            Symbol = sym;
+            Nazwa = naz;
+            LiczbaSztuk = lszt;
+            Magazyn = mag;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3}", Symbol, Nazwa, LiczbaSztuk,
+            Magazyn);
+        }
+    }
+}
